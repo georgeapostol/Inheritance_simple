26b31d47af22f8f7fa0f3c1899a01d0846198105
georgeapostol <gapostolopoulos@students.mchenry.edu>
2016-12-10 23:11:19 -0600

    Create inherit.py

---
+++ inherit.py
@@ -0,0 +1,17 @@
+from animal import *
+
+logan = Dog("Logan", "Terrier")
+
+print logan
+
+rabbit = Animal("Rabbit", True, "Herbivore")
+
+print rabbit
+
+Jacob = Dog("Jacob", "German Shephard")
+
+print Jacob
+
+snake = Animal("Snake", True, "Carnivore")
+
+print snake
